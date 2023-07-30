06830ed587495886f8d913cbbd836cba976fae52
whoyoux <48690153+whoyoux@users.noreply.github.com>
2023-07-30 16:36:01 +0200

    Added basic configuration

---
+++ src/config/site.ts
@@ -0,0 +1,14 @@
+export const siteConfig = {
+    name: 'Events Management',
+    description: 'App for events management.',
+    keywords: ['events management', 'events', 'management'],
+    url: 'https://url.com',
+    ogImage: 'https://url.com/opengraph-image.png',
+    authors: [
+        {
+            name: 'whoyoux',
+            url: 'https://github.com/whoyoux'
+        }
+    ],
+    creator: 'whoyoux'
+}
